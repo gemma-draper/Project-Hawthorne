db5fff7bc14857c1e1789d560daa6eb73cfc5818
Gemma Draper <g.l.draper@outlook.com>
2021-06-07 21:52:33 +0100

    Updates tipsy_scraper.py to use DataFrame. Scrapes all cocktails, names, urls.

--- tipsy_scraper.py
+++ tipsy_scraper.py
@@ -1,14 +1,18 @@
 # Web scraper for tipsybartender.com
 # %%
+import pandas as pd
+
 # initialise the web driver
 from selenium import webdriver
 from time import sleep
 driver = webdriver.Chrome()
 
-# Set the URL. Sleep to allow sit to load.
+# Set the URL. Sleep to allow site to load.
 driver.get('https://tipsybartender.com/drinks/all/')
-sleep(2)
+sleep(1)
 
+# get all category names and urls.
+# store in a list of dicts.
 drinks_categories = []
 category_dict = {}
 all_categories = driver.find_elements_by_xpath('//div[contains(@class, "subcollection-list")]/a')
@@ -18,10 +22,33 @@ for category in all_categories:
     category_dict['category'] = category.find_element_by_xpath('.//h5').text
     drinks_categories.append(category_dict.copy())
 
-# print(drinks_categories)
+all_drinks = pd.DataFrame()
+drink_info = {}
 
+for category in drinks_categories:
+    # go to the url and wait
+    driver.get(category['url'])
+    sleep(1)
+    # get the number of pages from the nav bar
+    number_of_pages = int(driver.find_element_by_xpath('//nav[@aria-label="Page navigation"]//li[last()-1]').text)
+    
+    for i in range(1,number_of_pages+1):
+        # iterate through all the pages in this category
+        driver.get(category['url'] + f'/page/{i}/')
+        #get all the drinks on this page
+        all_drinks_on_this_page = driver.find_elements_by_xpath('//div[contains(@class, "drink-card")]/a')
+        
+        for drink in all_drinks_on_this_page:
+            drink_info['category'] = category['category']
+            drink_info['name'] = drink.find_element_by_xpath('.//span').text
+            drink_info['url'] = drink.get_attribute('href')
+            all_drinks = all_drinks.append(drink_info,ignore_index=True)
+            
 
+        
 driver.quit()
+#%%
+all_drinks.head()
 
 # pokemons = driver.find_elements_by_xpath(‘/html/body/div[4]/section[5]/ul/li’)
 # pokemons_list = []
