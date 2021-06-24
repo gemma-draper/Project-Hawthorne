85ce819695643238910fc9f8efaa46f971f6ef35
Gemma Draper <g.l.draper@outlook.com>
2021-06-24 20:57:51 +0100

    Adds doc strings to all functions in all_recipes.py

--- all_recipes.py
+++ all_recipes.py
@@ -2,7 +2,7 @@ import re
 from pprint import pprint
 from driver_class import Driver
 
-#%%
+
 #init
 d = Driver()
 d.get('http://allrecipes.co.uk/recipes/drink-recipes.aspx')
@@ -29,40 +29,58 @@ for i in range(clean_page_count):
 
 print(drinks_from_all_recipes)
 
-drink_dict = drinks_from_all_recipes[1]
-d.get(drink_dict['url'])
-
 def get_ingredients(drink_dict=drink_dict):
+    """
+    Scrapes drink ingredients.
+    Takes drink_dict, returns drink_dict with drink_dict['ingredients'] = ingredients_dict.
+    """
     all_ingredients = d.find_elements('//*[@itemprop="ingredients"]')
+    ingredients_dict = {}
     for i in range(len(all_ingredients)):
         key = "ingredient_" + str(i)
         value = all_ingredients[i].text
-        drink_dict[key] = value
+        ingredients_dict[key] = value
+    drink_dict['ingredients'] = ingredients_dict
     return drink_dict
 
 #get yield
 def get_yield(drink_dict=drink_dict):
+    """
+    Scrapes recipe yield.
+    Takes drink_dict, returns it with recipe_yield.
+    """
     recipe_yield = d.find_element('//*[@itemprop="recipeYield"]/span')
     drink_dict['recipe_yield'] = recipe_yield.text
     return drink_dict
 
 #get description
 def get_description(drink_dict=drink_dict):
+    """
+    Scrapes recipe description.
+    Takes drink_dict, returns it with description.
+    """
     recipe_description = d.find_element('//p[@itemprop="description"]')
     drink_dict['description'] = recipe_description.text
     return drink_dict
 
 #get method
 def get_method(drink_dict=drink_dict):
+    """
+    Scrapes recipe method.
+    Takes drink_dict, returns it with drink_dict['method'] = method_dict.
+    """
     full_method =  d.find_elements('//*[@itemprop="recipeInstructions"]/li')
+    method_dict = {}
     for i in range(len(full_method)):
         key = "step_" + str(i)
         value = full_method[i].find_element_by_xpath('./span').text
-        drink_dict[key] = value
+        method_dict[key] = value
+    drink_dict['method'] = method_dict
     return drink_dict
 
-def clean_star_rating(messy=messy_star_rating):
-    """Takes a string containing star rating.
+def clean_star_rating(messy):
+    """
+    Takes a string containing star rating.
     Removes all non-numeric chars.
     Returns star rating as float.
     """
@@ -73,21 +91,34 @@ def clean_star_rating(messy=messy_star_rating):
         clean = float(messy)
     return clean
 
-#get star rating
+#get star rating and number of reviews
 def get_n_reviews_and_rating(drink_dict=drink_dict):
+    """
+    Scrapes star rating and number of reviews.
+    Takes drink_dict and returns it with star_rating and n_reviews.
+    """
     star_rating_box = d.find_element('//span[contains(@class, "mediumStar")]')
+    
     messy_star_rating = star_rating_box.get_attribute('class')
     messy_n_reviews = star_rating_box.find_element_by_xpath('../*[contains(@id, "Count")]').text
 
-    drink_dict['star_rating'] = clean_star_rating()
+    drink_dict['star_rating'] = clean_star_rating(messy_star_rating)
     drink_dict['n_reviews'] = int(messy_n_reviews.lstrip('( ').rstrip(') '))
     return drink_dict
 
-drink_dict = get_ingredients()
-drink_dict = get_yield()
-drink_dict = get_description()
-drink_dict = get_method() 
-drink_dict = get_n_reviews_and_rating()
+def get_time(drink_dict=drink_dict):
+    """
+    Scrapes preparation time.
+    Takes drink_dict and returns it with prep_time.
+    """
+
+for drink_dict in drinks_from_all_recipes:
+    d.get(drink_dict['url'])
+    drink_dict = get_ingredients()
+    drink_dict = get_yield()
+    drink_dict = get_description()
+    drink_dict = get_method() 
+    drink_dict = get_n_reviews_and_rating()
 
 
 from pprint import pprint
