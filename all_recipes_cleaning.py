5c7b0df24d8cb9f747bf1d6fcb4dc967eea322ea
gemma-draper <g.l.draper@outlook.com>
2021-06-27 14:23:56 +0100

    Adds more data cleaning

--- all_recipes_cleaning.py
+++ all_recipes_cleaning.py
@@ -9,18 +9,34 @@ with open('all_recipes_df_data.txt') as f:
 
 # convert to a dataframe
 df = pd.read_json(json_data, orient='columns')
-# %%
 
-df.describe()
 # %%
 # look at the data types and non-null count.
 df.info()
 # mostly type object, some float or int.
 # no cols are entirely null
+
 # %%
-# inspect n_ratings values
+# inspect numeric cols
+df.describe()
+# inspect n_ratings values, which has a high max
 df['n_ratings'].value_counts()
 # check the high numbers of ratings are real.
 df.loc[df['n_ratings'] > 1000]
 # I looked at the webpages: they are real!
+# %%
+# inspect recipe_yield, also has a high max
+df['recipe_yield'].value_counts()
+# check the high numbers of ratings are real.
+# lots with high numbers. 
+# Looked one with 750, actually 750ml not 750 servings.
+
+#%%
+# Dropping this col for now.
+# Will re-scrape this site to get the full string instead of stripping it.
+df.drop('recipe_yield', inplace=True, axis=1)
+
+# %%
+df.head()
+
 # %%
